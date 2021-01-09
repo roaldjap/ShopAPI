03b78f3dce42cde79eb8a5245cf92ec6d1d88e47
Jap Ignacio <Jap@macbook>
2021-01-09 16:41:32 +1300

    format indention

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,21 +1,19 @@
 class Api::V1::TransactionsController < ApplicationController
+  def get_all_apns
+    @apn_codes = Transaction.select(:apn_code).group(:apn_code).having("count(*) >= 1")
+    apn_only = @apn_codes.to_json(:only => [:apn_code])
+    render json: apn_only
+  end
 
-    def get_all_apns
-        @apn_codes = Transaction.select(:apn_code).group(:apn_code).having("count(*) >= 1")
-        apn_only = @apn_codes.to_json(:only => [:apn_code])
-        render json: apn_only
-    end
+  def get_dates
+    # 2770000000062
+    @trans_date = Transaction.select([:trans_date]).where(:apn_code => params[:apn_code])
+    trans_date = @trans_date.to_json(:only => [:trans_date])
+    render json: trans_date
+  end
 
-    def get_dates
-        # 2770000000062
-        @trans_date = Transaction.select([:trans_date]).where(:apn_code => params[:apn_code])
-        trans_date = @trans_date.to_json(:only => [:trans_date])
-        render json: trans_date
-    end
-
-    def show
-        @transaction = Transaction.where(:apn_code => params[:apn_code])
-        render json: @transaction
-    end
-    
+  def show
+    @transaction = Transaction.where(:apn_code => params[:apn_code])
+    render json: @transaction
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-
   namespace :api do
     namespace :v1 do
-      get '/get_apns/', to: 'transactions#get_all_apns'
-      get '/transactions/:apn_code', to: 'transactions#show'
-      get '/transactions/:apn_code/dates/', to: 'transactions#get_dates'
+      get "/get_apns/", to: "transactions#get_all_apns"
+      get "/transactions/:apn_code", to: "transactions#show"
+      get "/transactions/:apn_code/dates/", to: "transactions#get_dates"
     end
   end
 
-  root to: 'static_pages#landing'
-  get 'static_pages/landing'
+  root to: "static_pages#landing"
+  get "static_pages/landing"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
