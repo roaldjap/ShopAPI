6c761a646e287a46622b156912139234c282c4a4
Jap Ignacio <Jap@macbook>
2021-01-11 10:35:08 +1300

    Added chartjs / chartkick dependecies

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+require("chartkick")
+require("chartjs")
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
