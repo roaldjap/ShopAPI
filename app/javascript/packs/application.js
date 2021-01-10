941fac714666949d580d2f5a84fddda87148bc5e
Jap Ignacio <Jap@macbook>
2021-01-11 10:55:36 +1300

    disable chartkick first

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-require("chartkick")
-require("chartjs")
+// require("chartkick")
+// require("chartjs")
 
 Rails.start()
 Turbolinks.start()
