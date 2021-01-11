c5a379e87bc1bea6d08efbcccc0b334296dc6ca3
Jap Ignacio <Jap@macbook>
2021-01-11 13:56:03 +1300

    update loader

--- config/webpack/loaders/vue.js
+++ config/webpack/loaders/vue.js
@@ -1,22 +1,28 @@
 module.exports = {
-  test: /\.vue(\.erb)?$/,
-  use: [
-    'vue-style-loader',
-    'css-loader',
-    {
-      loader: 'sass-loader',
-      // Requires sass-loader@^7.0.0
-      options: {
-        implementation: require('sass'),
-        indentedSyntax: true // optional
+  module: {
+    rules: [
+      {
+        test: /\.vue(\.erb)?$/,
+        use: [
+          "vue-style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            // Requires sass-loader@^7.0.0
+            options: {
+              implementation: require("sass"),
+              indentedSyntax: true, // optional
+            },
+            // Requires sass-loader@^8.0.0
+            options: {
+              implementation: require("sass"),
+              sassOptions: {
+                indentedSyntax: true, // optional
+              },
+            },
+          },
+        ],
       },
-      // Requires sass-loader@^8.0.0
-      options: {
-        implementation: require('sass'),
-        sassOptions: {
-          indentedSyntax: true // optional
-        },
-      },
-    },
-  ],
-}
+    ],
+  },
+};
