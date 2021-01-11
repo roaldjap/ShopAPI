e34a34196e22c1a8f30fd5fdcbc8b0d88486a9a3
Jap Ignacio <Jap@macbook>
2021-01-11 14:05:46 +1300

    revert back vueloader

--- config/webpack/loaders/vue.js
+++ config/webpack/loaders/vue.js
@@ -1,28 +1,6 @@
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.vue(\.erb)?$/,
-        use: [
-          "vue-style-loader",
-          "css-loader",
-          {
-            loader: "sass-loader",
-            // Requires sass-loader@^7.0.0
-            options: {
-              implementation: require("sass"),
-              indentedSyntax: true, // optional
-            },
-            // Requires sass-loader@^8.0.0
-            options: {
-              implementation: require("sass"),
-              sassOptions: {
-                indentedSyntax: true, // optional
-              },
-            },
-          },
-        ],
-      },
-    ],
-  },
-};
+  test: /\.vue(\.erb)?$/,
+  use: [{
+    loader: 'vue-loader'
+  }]
+}
